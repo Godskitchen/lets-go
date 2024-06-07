b74ae38134e52f80165f4f9a87d15ae9e624419b
Godskitchen <diemygod@gmail.com>
2024-06-08 00:11:11 +0300

    host update

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ fastify.get('/', async (_request, _reply) => ({ hello: 'world' }));
 
 // Run the server!
 try {
-  await fastify.listen({ port: 3000 });
+  await fastify.listen({ host: '0.0.0.0', port: 3000 });
 } catch (err) {
   fastify.log.error(err);
   throw new Error;
